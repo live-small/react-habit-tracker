1323c9ab573df888fbdb105fa96d71ef12294924
suhwa <hansuhwa2005@gmail.com>
2022-05-25 17:12:04 +0900

    fix: totalCount 상태값 제거 -> habits에서 계산한(파생된) 상태값이에 따로 관리할 필요가 없음
    이 상태값을 꼭 관리해야하는지, 어떤 상황에서 상태값이 변하는지 체크하기, 불필요한 상태값은 제거하기

--- src/app.jsx
+++ src/app.jsx
@@ -4,16 +4,14 @@ import NavBar from "./components/navbar";
 
 import React, { Component } from "react";
 
-export default class app extends Component {
+export default class App extends Component {
 	state = {
 		habits: [
 			{ id: 1, name: "Reading", count: 0 },
 			{ id: 2, name: "eating", count: 0 },
 			{ id: 3, name: "studying", count: 0 },
 		],
-		totalCount: 0,
 	};
-	// 필요한 상태값 : total count(실제로 하고 있는 습관 개수)
 
 	handleIncrement = (habit) => {
 		// 리액트는 state를 직접 수정하면 안된다. -> 불변성을 유지해야함!! 안그러면 버그가 많이 생김..
@@ -21,7 +19,6 @@ export default class app extends Component {
 		const key = habits.indexOf(habit);
 		habits[key].count++;
 		this.setState({ habits });
-		this.setState({ totalCount: this.getTotalCount(habits) });
 	};
 
 	handleDecrement = (habit) => {
@@ -33,7 +30,6 @@ export default class app extends Component {
 		};
 		habits.splice(key, 1, updateHabit);
 		this.setState({ habits });
-		this.setState({ totalCount: this.getTotalCount(habits) });
 	};
 
 	handleDelete = (habit) => {
@@ -41,11 +37,6 @@ export default class app extends Component {
 		const key = habits.indexOf(habit);
 		habits.splice(key, 1);
 		this.setState({ habits });
-		this.setState({ totalCount: this.getTotalCount(habits) });
-	};
-
-	getTotalCount = (habits) => {
-		return habits.filter((habit) => habit.count > 0).length;
 	};
 
 	handleAdd = (name) => {
@@ -57,7 +48,12 @@ export default class app extends Component {
 	render() {
 		return (
 			<>
-				<NavBar totalCount={this.state.totalCount} />
+				<NavBar
+					totalCount={
+						this.state.habits.filter((habit) => habit.count > 0)
+							.length
+					}
+				/>
 				<Habits
 					habits={this.state.habits}
 					onIncrement={this.handleIncrement}
