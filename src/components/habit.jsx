5473f8fe942b4fbb75634b3f2b23a144b5c0401a
suhwa <hansuhwa2005@gmail.com>
2022-05-25 14:00:43 +0900

    feat: 여러개의 Habit 관리하는 Habits 컴포넌트 구현
    Habit 카운터 이벤트 props로 넘겨 재사용

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -23,38 +23,30 @@ import React, { Component } from "react";
 
 // class 컴포넌트
 class Habit extends Component {
-	state = {
-		count: 0,
-	};
-
-	handleIncrement = () => {
-		this.setState({ count: this.state.count + 1 });
-		// setState안쓰면 리액트는 상태가 바뀐 걸 감지하지 못하고, 리렌더링 시켜주지 않는다
-	};
-
-	handleDecrement = () => {
-		const updateCount = this.state.count - 1;
-		this.setState({ count: updateCount < 0 ? 0 : updateCount });
-	};
-
 	render() {
+		const { onIncrement, onDecrement, onDelete, habit } = this.props;
+		const { name, count } = habit;
+
 		return (
 			<li className="habit">
-				<span className="habit-name">Reading</span>
-				<span className="habit-count">{this.state.count}</span>
+				<span className="habit-name">{name}</span>
+				<span className="habit-count">{count}</span>
 				<button
 					className="habit-button habit-increase"
-					onClick={this.handleIncrement}
+					onClick={() => onIncrement(habit)}
 				>
 					<i className="fa-solid fa-square-plus"></i>
 				</button>
 				<button
 					className="habit-button habit-decrease"
-					onClick={this.handleDecrement}
+					onClick={() => onDecrement(habit)}
 				>
 					<i className="fa-solid fa-square-minus"></i>
 				</button>
-				<button className="habit-button habit-delete">
+				<button
+					className="habit-button habit-delete"
+					onClick={() => onDelete(habit)}
+				>
 					<i className="fa-solid fa-trash"></i>
 				</button>
 			</li>
---
+++ src/components/habits.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import Habit from "./habit";
+
+export default class Habits extends Component {
+	state = {
+		habits: [
+			{ id: 1, name: "Reading", count: 0 },
+			{ id: 2, name: "eating", count: 0 },
+			{ id: 3, name: "studying", count: 0 },
+		],
+	};
+
+	handleIncrement = (habit) => {
+		// 리액트는 state를 직접 수정하면 안된다. -> 불변성을 유지해야함!! 안그러면 버그가 많이 생김..
+		const habits = [...this.state.habits];
+		const key = habits.indexOf(habit);
+		habits[key].count++;
+		this.setState({ habits });
+	};
+
+	handleDecrement = (habit) => {
+		const key = this.state.habits.indexOf(habit);
+		const updateHabit = {
+			...habit,
+			count: habit.count - 1 < 0 ? 0 : habit.count - 1,
+		};
+		this.setState(this.state.habits.splice(key, 1, updateHabit));
+	};
+
+	handleDelete = (habit) => {
+		const habits = [...this.state.habits];
+		const key = habits.indexOf(habit);
+		habits.splice(key, 1);
+		this.setState({ habits });
+	};
+
+	render() {
+		return (
+			<ul>
+				{this.state.habits.map((habit) => (
+					<Habit
+						key={habit.id}
+						habit={habit}
+						onIncrement={this.handleIncrement}
+						onDecrement={this.handleDecrement}
+						onDelete={this.handleDelete}
+					/>
+				))}
+			</ul>
+		);
+	}
+}
